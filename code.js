c08b7ee5155f484c7a77471461f908317534babe
sixi1116 <hengai@outlook.com>
2022-11-16 18:31:35 +0800

    test

---
+++ code.js
@@ -0,0 +1,15 @@
+var word="bottle";
+var count=10;
+while (count>0){
+
+    console.log(count+" "+word+" of beer on the wall");
+    console.log(count+" "+word+" of beer,");
+    console.log("take one down ,pass it around");
+    count=count-1;
+}
+if (count>1){
+        console.log(count+" "+word+" of beer on the wall");
+    }
+    else {
+        console.log("no more"+word+" of beer on the wall");
+    }
---
+++ code1.js
@@ -0,0 +1,14 @@
+// 函数function
+function test(name,weight){
+    if (weight>20){
+        console.log(name+" WOOF")
+    }else{
+        console.log(name+" woof")
+    }
+}
+test("a",20);
+test("b",-1);
+test("c",0,0);
+test("d","20");
+test("e",20);
+test("f",30);
