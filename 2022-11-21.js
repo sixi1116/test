0de8a6d3128e459e215b53ef27f6e953c5c91dce
sixi1116 <hengai@outlook.com>
2022-11-21 21:09:54 +0800

    test

--- 2022-11-21.js
+++ 2022-11-21.js
@@ -1,3 +1,28 @@
-var plant=document.getElementById("greenplant")
-plant.innerHTML="red alert:hit by phaser fire"
-console.log(plant.innerHTML)
+// var plant=document.getElementById("greenplant")
+// plant.innerHTML="red alert:hit by phaser fire"
+// console.log(plant.innerHTML)
+
+var subject="juest a string";
+var test1="ab";
+var test2=12;
+var test3=true;
+var test4={}; 
+var test5=[];
+var test6;
+var test7={"ab":12};
+var test8=["ab",12];
+function test9(){
+    return "123";
+}
+// var probe=typeof subject;
+// console.log(typeof subject);
+console.log(typeof subject);
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
